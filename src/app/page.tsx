bd2f0511c9e045a30606bbe3a183b3970f4bd857
arijitdas122001 <aribwnlvpgbwn@gmail.com>
2024-04-09 23:08:03 +0530

    sticky roll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import Featured from "@/components/Featured";
+import HeroSection from "@/components/HeroSection";
+import Whychooseus from "@/components/Whychooseus";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Music academy
+        <HeroSection/>
+        <Featured/>
+        <Whychooseus/>
     </main>
   );
 }
