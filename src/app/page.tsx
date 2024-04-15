2d3beb3c081122ffb214ed148546800269815932
arijitdas122001 <aribwnlvpgbwn@gmail.com>
2024-04-16 01:41:45 +0530

    wavy background,animated card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import Featured from "@/components/Featured";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MovingCards from "@/components/MovingCards";
 import Whychooseus from "@/components/Whychooseus";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection/>
         <Featured/>
         <Whychooseus/>
         <MovingCards/>
+        <Instructors/>
+        <Footer/>
     </main>
   );
 }
